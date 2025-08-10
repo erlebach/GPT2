310062b24c2ab45f0cae21d9b21ccac2f24af043
erlebach <gordon.erlebach@gmail.com>
2025-08-10 19:41:45 -0400

    Forgotten file.

---
+++ run_hydra.py
@@ -0,0 +1,5 @@
+"""Run memory experiments using @hydra_runner."""
+
+from src.experiments.memory_measurements import measure_memory_scaling_experiments_hydra
+
+measure_memory_scaling_experiments_hydra()
